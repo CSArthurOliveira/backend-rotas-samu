041f577b46b2b56a7ed738ed426773514d26419b
Arthur Oliveira <arthurnathanoliveiraleite@hotmail.com>
2025-12-10 22:11:01 -0300

    Initial Commit

--- src/main/java/br/com/uern/rotas_samu/config/CorsConfig.java
+++ src/main/java/br/com/uern/rotas_samu/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "https://rotas-samu.vercel.app/"
+                        "https://rotas-samu.vercel.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
